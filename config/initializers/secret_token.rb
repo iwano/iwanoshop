8aed4714595902a332a4d08e8de4f553cfdb9353
Ivan Calderon <iwano@hotmail.com>
2012-06-05 18:47:35 -0500

    Generated my application using the shopify_app gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopifyApp::Application.config.secret_token = '63842d18572ae904aebf612a677360e5739e3c69051b2b67a7b0c9f79eafef453f3fdec9b1b1b20a6125d46311b3fa15dd9fbcfb8579cbc592c9abbb3f0188f6'
